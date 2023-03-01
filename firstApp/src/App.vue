3bdb7591b350239dc2f6ac088c04ad4d6d647843
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-01 12:44:32 +0900

    変数を表示、計算、条件分岐させる簡単な紹介サンプルコード
    	App.vueを触ってみる。変数は破壊的ではないようだ。
    		<script setup>
    			console.log("hello vue!!");
    			const message = "yes it is";
    		</script>
    
    		<template>
    		  <h1>Vue 3 入門</h1>
    		  <p>{{ message.length * 10 }}</p>
    		  <p>{{ message.length * 10 > 10 ? 'long' : 'short' }}</p>
    		  <p>{{ message.length > 10 ? 'long' : 'short' }}</p>
    		</template>

---
+++ firstApp/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+console.log("hello vue!!");
+const message = "yes it is";
+</script>
+
+<template>
+  <h1>Vue 3 入門</h1>
+  <p>{{ message.length * 10 }}</p>
+  <p>{{ message.length * 10 > 10 ? 'long' : 'short' }}</p>
+  <p>{{ message.length > 10 ? 'long' : 'short' }}</p>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
