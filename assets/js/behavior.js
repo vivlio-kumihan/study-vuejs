79bf59c4a43214a45938e00a644043bdfcfe2529
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-03 21:34:31 +0900

    Mustache構文とディレクティブ

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,7 +1,15 @@
 Vue.createApp({
     data: () => {
         return {
-            message: "hello, vue!"
+            message: "hello, vue!",
+            url: "https://kumihan.com",
+            flag: false
         }
     }
 }).mount("#app")
+
+const input = document.querySelector("input")
+input.addEventListener("click", () => {
+    const outPut = document.querySelector("dd")
+    outPut.textContent = "hello, world!"
+})
