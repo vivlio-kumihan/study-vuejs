c4c6e539a5f68e69eb23508b312d0e9e1ef40b67
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-12 20:26:53 +0900

    v-on:click=''

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,13 +1,12 @@
-Vue.createApp({
-    data: function() {
-        return {
-            email: "vivlio@kumihan.com"
-        }
-    },
-    computed: {
-        localEmail: function() {
-            const str = this.email.split("@")[0]
-            return str.codePointAt(0).toString(16)
-        }
+Vue.createApp ({
+  data() {
+    return {
+      localTime: ""
     }
-}).mount("#app");
+  },
+  methods: {
+    onclick() {
+      this.localTime = new Date().toLocaleDateString()
+    }
+  }
+}).mount("#app")
