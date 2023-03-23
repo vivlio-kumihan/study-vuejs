fe90075ae496ca5cc637df986ac501bc7c1009c0
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-24 08:39:38 +0900

    radio butto, vue and js
    	複数選択
    	vue => v-modelの値を揃える
    	js  => name属性を揃える
    	radio buttonの属性を収集してforで回しながらcheckedされたものを返す作戦。
    同じことをするのにコード量が違いすぎる。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,10 +1,30 @@
+// vue
 Vue.createApp({
   data() {
     return {
       pet: "選択したのは...",
-      agree: true,
-      application: [],
-      os: ["Windows"]
+      food: "選択したのは...",
+      subject: "選択したのは..."
     }
   }
 }).mount("#app")
+
+// js
+function getRadioValue(name) {
+  let result = ""
+  let elements = document.querySelectorAll(`input[name="${name}"]`)
+  for (let elem of elements) {
+    if (elem.checked) {
+      result = elem.value
+      break
+    }
+  }
+  return result
+}
+
+document.querySelector("#btn").addEventListener("click", function () {
+  document.querySelector("#for-js").textContent = getRadioValue("subject")
+  console.log(getRadioValue("subject"))
+}, false);
+
+  
