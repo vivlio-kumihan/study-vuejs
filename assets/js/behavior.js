708f52fb42d1a0f480a2baf1b0f2dea93135dad8
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-25 16:53:22 +0900

    check box, vue and js
    	=> vue.jsとVanilla.jsを混ぜると訳がわからなくなる。
    	   というか、vue.jsで処理した方が良い。
    	=> formをdl, dt, ddでラップする方法について疑問あり。
    		・addEventListenerのきっかけになる要素を『dd』にしていいのか？
    		・送信ボタンをどう設置すればいいかわからない。
    		　ボタンを押した瞬間にコンソールに値は現れるがすぐに消えてしまう。
    		　画面の効果も同じく変化を維持しない。
    	=> vue.jsでの、値のコントロールが上手くいかない。
    		・コンソールに配列は表示できる。でもこれをスクリーン上で文字列に変換して
    		　表示したい場合、methodsを使うと思うのだが上手く設置できない。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,46 +1,38 @@
-// vue
 Vue.createApp({
   data() {
     return {
-      lang: "選択はしたものは...",
-      application: []
+      agree: "チェックの有無",
+      application: ["JavaScript"]
     }
   }
 }).mount("#app")
 
-// js
-// for single
-function getSelectValue(name) {
-  let result = ""
-  let options = document.querySelector(name).options
+// // Vanilla-js single
+// const agree = document.getElementById("yes")
+// let singleCheckBox = document.querySelector("#vanilla-js .single")
+// document.querySelector(".single .reply.btn").addEventListener("click", function(){
+//   if(agree.checked) {
+//     singleCheckBox.lastElementChild.textContent = agree.value
+//   } else {
+//     singleCheckBox.lastElementChild.textContent = "希望しません。"
+//   }
+// }, false)
 
-  for(let opt of options) {
-    if(opt.selected) {
-      result = opt.value
-      break
-    }
-  }
-  return result
-}
-
-document.querySelector(".result.single > input").addEventListener("click", function() {
-  document.querySelector("#case-js .result.single > p").textContent = getSelectValue("#case-js #lang.apply")
-})
-
-// for multipul
-function getSelectValueArr(name) {
-  let result = []
-  let options = document.querySelector(name).options;
-
-  for(let opt of options) {
-    if(opt.selected) {
-      result.push(opt.value)
-    }
-  }
-  return result
-}
-
-document.querySelector(".result.multiple > input").addEventListener("click", function() {
-  const reply = getSelectValueArr("#case-js #lang.os").join("・")
-  document.querySelector("#case-js .result.multiple > p").textContent = reply
-}, false)
+// // Vanilla-js multiple
+// let multipleCheckBox = document.querySelector(".multiple")
+// document.querySelector(".multiple .reply.btn").addEventListener("click", function() {
+//   let result = []
+//   const subjects = document.querySelectorAll('input[name="study"]')
+//   for (let subject of subjects) {
+//     if (subject.checked) {
+//       result.push(subject.value)
+//     }
+//   }
+//   // console.log(result)
+//   if (result.length >= 1) {
+//     let str = result.join(" / ")
+//     multipleCheckBox.lastElementChild.textContent = str
+//   } else {
+//     multipleCheckBox.lastElementChild.textContent = "希望しません。"
+//   }
+// }, false)
