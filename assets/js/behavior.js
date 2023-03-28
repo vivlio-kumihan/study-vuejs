7e7938242feeac91d7d6d5d9be8156fec3097c2f
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-28 15:28:43 +0900

    オブジェクトをバインドする
    	問題点 => 初期値をformに掲示しておく方法が分からない。
    	v=bind:属性 => 属性にJavaScriptを埋め込む
    		わかりにくい言い方。要は属性にvueで設定した変数を埋め込み方法
    	問題点 =>
    	v=on:click="method name"
    		アンカーをクリックしたら『target="_brank"』を付与したい。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,38 +1,18 @@
 Vue.createApp({
   data() {
     return {
-      str: "配列を文字列で表示させる。",
-      arrDigit: [1, 2, 3, 4],
-      arrName: ["信之", "和恵", "茉李"]
-    }
-  },
-  computed: {
-    toStringOne: function() {
-      return this.arrDigit.join("-")
+      unit: "",
+      uri: "https://kumihan.com"
+
     }
   },
   methods: {
-    toStringTwo: function() {
-      // // for of version
-      // let tmpArr = []
-      // for (let firstName of this.arrName) {
-      //   let fullName = `高広${firstName}`
-      //   tmpArr.push(fullName)
-      // }
-      // return fullName.join("、")
-
-      // // forEach version
-      // let fullName = []
-      // this.arrName.forEach(firstName => {
-      //   fullName.push("高広" + firstName)        
-      // })
-      // return fullName.join("、")
-      
-      // map version
-      const fullName = this.arrName.map(firstName => {
-        return "高広" + firstName
-      });
-      return fullName.join("、")
+    onchange() {
+      return `${this.unit.name}: ${this.unit.size}`
+    },
+    targetBrank() {
+      const link = document.querySelector("dd > a").setAttribte("target", "_blank")
+      return link
     }
   }
 }).mount("#app")
