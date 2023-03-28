b2fb5a3e70ca5d981640b7d3c5433b11c3736b6e
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-28 11:54:12 +0900

    methods => 配列を文字列に変換するして出力

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,38 +1,38 @@
 Vue.createApp({
   data() {
     return {
-      agree: "チェックの有無",
-      application: ["JavaScript"]
+      str: "配列を文字列で表示させる。",
+      arrDigit: [1, 2, 3, 4],
+      arrName: ["信之", "和恵", "茉李"]
+    }
+  },
+  computed: {
+    toStringOne: function() {
+      return this.arrDigit.join("-")
+    }
+  },
+  methods: {
+    toStringTwo: function() {
+      // // for of version
+      // let tmpArr = []
+      // for (let firstName of this.arrName) {
+      //   let fullName = `高広${firstName}`
+      //   tmpArr.push(fullName)
+      // }
+      // return fullName.join("、")
+
+      // // forEach version
+      // let fullName = []
+      // this.arrName.forEach(firstName => {
+      //   fullName.push("高広" + firstName)        
+      // })
+      // return fullName.join("、")
+      
+      // map version
+      const fullName = this.arrName.map(firstName => {
+        return "高広" + firstName
+      });
+      return fullName.join("、")
     }
   }
 }).mount("#app")
-
-// // Vanilla-js single
-// const agree = document.getElementById("yes")
-// let singleCheckBox = document.querySelector("#vanilla-js .single")
-// document.querySelector(".single .reply.btn").addEventListener("click", function(){
-//   if(agree.checked) {
-//     singleCheckBox.lastElementChild.textContent = agree.value
-//   } else {
-//     singleCheckBox.lastElementChild.textContent = "希望しません。"
-//   }
-// }, false)
-
-// // Vanilla-js multiple
-// let multipleCheckBox = document.querySelector(".multiple")
-// document.querySelector(".multiple .reply.btn").addEventListener("click", function() {
-//   let result = []
-//   const subjects = document.querySelectorAll('input[name="study"]')
-//   for (let subject of subjects) {
-//     if (subject.checked) {
-//       result.push(subject.value)
-//     }
-//   }
-//   // console.log(result)
-//   if (result.length >= 1) {
-//     let str = result.join(" / ")
-//     multipleCheckBox.lastElementChild.textContent = str
-//   } else {
-//     multipleCheckBox.lastElementChild.textContent = "希望しません。"
-//   }
-// }, false)
