3f6847d19dfb05d82ef49654098e96294ffa01b2
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-28 17:40:56 +0900

    v-if, v-else
    	v-if="show", v-else => 条件分岐でクリックしたら表示させる挙動が簡単に生成。
    		表示させるは"show"、隠れている時の表示は"v-else"で指定する。
    
    	CSSの知識 :first-of-type, last-of-type, first-child, last-child =>
    		=> first-of-type, last-of-type
    			コンテナ以下にある『指定した要素』の最初（最後）を選択する。
    		=> first-child, last-child
    			コンテナ以下の『最初（最後）』の要素を選択する。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,18 +1,7 @@
 Vue.createApp({
   data() {
     return {
-      unit: "",
-      uri: "https://kumihan.com"
-
-    }
-  },
-  methods: {
-    onchange() {
-      return `${this.unit.name}: ${this.unit.size}`
-    },
-    targetBrank() {
-      const link = document.querySelector("dd > a").setAttribte("target", "_blank")
-      return link
+      show: true
     }
   }
 }).mount("#app")
