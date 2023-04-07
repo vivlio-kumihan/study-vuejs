88218645043b7b99ec63b6e676cd7e0d68619b42
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-07 17:25:24 +0900

    formの入力をHTMLへ出力する。Vanilla.jsとVueでやる。
    	未完成：
    	Vue => ボタンを押して送信後に表示させる。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,11 +1,29 @@
+// formにテキスト入力をHTMLへ出力する
+// Vannira.js version
+// document.getElementById("form").onsubmit = function (event) {
+//   event.preventDefault()
+//   // console.log(event)
+//   let inPutText = document.getElementById("inPutText").value
+//   let outPutText = document.querySelector("#out-put-text")
+//   outPutText.textContent = `入力されたものは『${ inPutText }』ですね。`
+// }
+
 Vue.createApp({
+  // data: function() {
   data() {
     return {
-      map: new Map([
-        ['PHP', 'PHP: Hypertext Preprocessor'],
-        ['JSP', 'Jakarta Server Pages'],
-        ['ASP', 'Active Server Pages']
-      ])
+      inputText: "入力"
     }
   }
 }).mount("#app")
+// Vue.createApp({
+//   data() {
+//     return {
+//       map: new Map([
+//         ['PHP', 'PHP: Hypertext Preprocessor'],
+//         ['JSP', 'Jakarta Server Pages'],
+//         ['ASP', 'Active Server Pages']
+//       ])
+//     }
+//   }
+// }).mount("#app")
