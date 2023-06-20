d10e07c818a7bcb39abdbc85d579414e1a4ceaa5
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-06-20 21:15:33 +0900

    変数、配列、ハッシュ、mounted: function() {}の設定法について

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,7 +1,33 @@
-Vue.createApp({
+const app = ({
+  // data: function() {
   data() {
     return {
-      myName: ""
+      // ここで生成したインスタンスをHTMLで出力できる。
+      // textContentみたいなもの
+      text: 'hello',
+      // 変数
+      a: 1,
+      // 配列
+      arr: ['a', 'b'],
+      // 連想配列
+      hash: {
+        name: 'nobuyuki'
+      }
     }
+  },
+  // ライフサイクル・メソッド
+  // 上の処理が終わったらこちらの処理をする
+  // ここで、JavaScriptを書ける
+  // mounted: function() {
+  mounted() {
+    // 関数を定義して。。。
+    function greet(arg = 'hello') {
+      console.log(arg)
+    }
+    // コンソールへ出力してみる。
+    greet('good night')
+    // ここのインスタンスは『this』
+    console.log(this.text)
   }
-}).mount("#app")
+})
+Vue.createApp(app).mount('#app')
