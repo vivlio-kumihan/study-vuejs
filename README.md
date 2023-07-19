2f3443ddc1328b9cba9926a6ac1e3a671a10861a
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-07-20 06:04:22 +0900

    watch

--- README.md
+++ README.md
@@ -683,3 +683,72 @@ Vue.createApp({
   </ul>
 </div>
 ```
+
+## watch
+
+```html
+  <div id="app" class="app">
+    <button @click="count += 2">button</button>
+    <p>{{ count }}</p>
+
+    <!-- オブジェクトを操作する際の注意 -->
+    <!-- <button @click="info.name = '和恵'">button</button>
+    <p>{{ info.name }}</p> -->
+    
+    <button @click="info.name = '和恵'">button</button>
+    <p>{{ info.name }}</p>
+  </div>
+</body>
+```
+
+```js
+Vue.createApp({
+  data() {
+    return {
+      count: 0,
+      info: {
+        name: '信之'
+      }
+    }
+  },
+  watch: {
+    // 変化した後の値が引数に入る。
+    // 引数を与えない場合、methodsで定義した
+    // メソッドと同じ反応をする。
+    // つまり、『count』を出力し続ける。
+    count() {
+      console.log('count')
+    }
+
+    // 引数を与えると計算し終わった値を
+    // 出力する。
+    count(arg) {
+      console.log(arg)
+    }
+
+    // 計算元と計算後の値を引数にできる。
+    count(newValue, oldValue) {
+      console.log(newValue, oldValue)
+    }
+
+    // オブジェクトを操作する際の注意
+    // クリックするとHTMLで与えた値に変更はできるが、
+    // こちらで定義している内容は無視される。
+    info() {
+      console.log('情報を更新しました。')
+    }
+    
+    info: {
+      handler() {
+        console.log('情報を更新しました。')
+      },
+      // オブジェクトの値に変更があった場合に
+      // 定義した内容を実行できるオプション。
+      deep: true,
+      // HTML上で値の変更をする前に
+      // 定義した内容を実行するオプション。
+      immediate: true
+    }
+  }
+}).mount('#app')
+```
--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,29 +1,49 @@
 Vue.createApp({
   data() {
     return {
-      fruits: [{
-        name: 'apple',
-        option: [
-          { size: 'large', price: 200 },
-          { size: 'middle', price: 150 },
-          { size: 'small', price: 100 }
-        ]
-      }, {
-        name: 'banana',
-        option: [
-          { size: 'large', price: 300 },
-          { size: 'middle', price: 350 },
-          { size: 'small', price: 200 }
-        ]
-      }, {
-        name: 'orange',
-        option: [
-          { size: 'large', price: 400 },
-          { size: 'middle', price: 450 },
-          { size: 'small', price: 300 }
-        ]
-      }]
+      count: 0,
+      info: {
+        name: '信之'
+      }
+    }
+  },
+  watch: {
+    // // 変化した後の値が引数に入る。
+    // // 引数を与えない場合、methodsで定義した
+    // // メソッドと同じ反応をする。
+    // // つまり、『count』を出力し続ける。
+    // count() {
+    //   console.log('count')
+    // }
+
+    // // 引数を与えると計算し終わった値を
+    // // 出力する。
+    // count(arg) {
+    //   console.log(arg)
+    // }
+
+    // // 計算元と計算後の値を引数にできる。
+    // count(newValue, oldValue) {
+    //   console.log(newValue, oldValue)
+    // }
+
+    // // オブジェクトを操作する際の注意
+    // // クリックするとHTMLで与えた値に変更はできるが、
+    // // こちらで定義している内容は無視される。
+    // info() {
+    //   console.log('情報を更新しました。')
+    // }
+    
+    info: {
+      handler() {
+        console.log('情報を更新しました。')
+      },
+      // オブジェクトの値に変更があった場合に
+      // 定義した内容を実行できるオプション。
+      deep: true,
+      // HTML上で値の変更をする前に
+      // 定義した内容を実行するオプション。
+      immediate: true
     }
   }
 }).mount('#app')
-
