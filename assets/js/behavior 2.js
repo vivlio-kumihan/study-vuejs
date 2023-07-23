b920839bcb82f5025dcdfefd7123886d7536181b
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-07-24 06:24:46 +0900

    v-model input要素 checkbox, radio button, selectbox

--- assets/js/behavior 2.js
+++
@@ -1,9 +0,0 @@
-const app = ({
-  data() {
-    return {
-      range: 50,
-      color: '#555'
-    }
-  }
-})
-Vue.createApp(app).mount('#app')
--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,8 +1,18 @@
 const app = ({
   data() {
     return {
-      range: 50,
-      color: '#555'
+      // ボタンを選択する前の状態はfalseが定石
+      flag: false,
+      age: [],
+      areaList: [
+        '大阪',
+        '京都',
+        '神戸',
+      ],
+      // 空の文字列でもいいが、
+      // 属性checkedの代わりに初期値を設定する。
+      checkedArea: '大阪',
+      selectedArea: '大阪',
     }
   }
 })
