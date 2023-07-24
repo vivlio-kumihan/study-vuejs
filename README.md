fc46bbd6fcfe18e995dc63a4ab8f4f7e302653b8
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-07-24 21:48:08 +0900

    ライフサイクルメソッド

--- README.md
+++ README.md
@@ -1278,3 +1278,65 @@ Vue.createApp(app).mount('#app')
   </div>
 </body> 
 ```
+
+
+## ライフサイクルメソッド
+
+決められたタイミングで実行される関数。
+
+```html
+<div id="app" class="app">
+  <p>{{ count }}</p>
+  <button @click="countUp">count up!</button>
+</div>
+```
+
+```js
+const app = ({
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    countUp() {
+      this.count++
+    }
+  },
+  beforeCreate() {
+    // Vue.jsを初期化する前に起動するメソッド
+    // Vueのインスタンスができる前で変数の初期化やeventListenerなどはない状態。
+    console.log('before create ')
+    console.log(this.count) // => undefineになる。
+  },
+  created() {
+    // Vue.jsが初期下した直後に起動するメソッド
+    console.log('created')
+    console.log(this.count) // => 0
+    // ただし、HTMLに紐づいていない状態。　
+    console.log(document.querySelector('p')) // => null
+  },
+  beforeMount() {
+    // Vue.jsのインスタンスがHTMLに紐づく直前。
+    console.log('before mount')
+    console.log(document.querySelector('p')) // => null
+  },
+  mounted() {
+    // HTMLにアクセスできる。
+    console.log('mounted')
+    console.log(document.querySelector('p')) // => <p>0</p>
+  },
+  // ボタンをクリックすると発火する直前。
+  // データがアップデートされる前に実行する。
+  beforeUpdate() {
+    console.log('before update')
+  },
+  // ボタンをクリックすると発火する直後。
+  // データがアップデートされた後に実行する。
+  updated() {
+    console.log('updated')
+  },
+})
+
+Vue.createApp(app).mount('#app')
+```
--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,19 +1,47 @@
 const app = ({
   data() {
     return {
-      // ボタンを選択する前の状態はfalseが定石
-      flag: false,
-      age: [],
-      areaList: [
-        '大阪',
-        '京都',
-        '神戸',
-      ],
-      // 空の文字列でもいいが、
-      // 属性checkedの代わりに初期値を設定する。
-      checkedArea: '大阪',
-      selectedArea: '大阪',
+      count: 0
     }
-  }
+  },
+  methods: {
+    countUp() {
+      this.count++
+    }
+  },
+  beforeCreate() {
+    // Vue.jsを初期化する前に起動するメソッド
+    // Vueのインスタンスができる前で変数の初期化やeventListenerなどはない状態。
+    console.log('before create ')
+    console.log(this.count) // => undefineになる。
+  },
+  created() {
+    // Vue.jsが初期下した直後に起動するメソッド
+    console.log('created')
+    console.log(this.count) // => 0
+    // ただし、HTMLに紐づいていない状態。　
+    console.log(document.querySelector('p')) // => null
+  },
+  beforeMount() {
+    // Vue.jsのインスタンスがHTMLに紐づく直前。
+    console.log('before mount')
+    console.log(document.querySelector('p')) // => null
+  },
+  mounted() {
+    // HTMLにアクセスできる。
+    console.log('mounted')
+    console.log(document.querySelector('p')) // => <p>0</p>
+  },
+  // ボタンをクリックすると発火する直前。
+  // データがアップデートされる前に実行する。
+  beforeUpdate() {
+    console.log('before update')
+  },
+  // ボタンをクリックすると発火する直後。
+  // データがアップデートされた後に実行する。
+  updated() {
+    console.log('updated')
+  },
 })
+
 Vue.createApp(app).mount('#app')
